75c49dce5ca7d098a37804b0441f57b3dd3e1767
JTXSZMSD <91826944+JTXSZMSD@users.noreply.github.com>
2023-08-20 23:46:01 +0300

    Tailor items update

--- Professions/TailorItem.cs
+++ Professions/TailorItem.cs
@@ -4,10 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ArcheAgeHelper.Professions
+namespace ArcheAgeHelper
 {
-    internal class BlacksmithItem : ItemClass
+    internal class TailorItem
     {
-
     }
 }
